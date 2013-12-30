030799f4ac6558ae4085168ad2265f4b29c68d65
Dennis Dalessandro <dennis.dalessandro@intel.com>
2013-12-30 10:04:48 -0500

    compat: qib changes for 3.12
    Signed-off by Dennis Dalessandro (dennis.dalessandro@intel.com)

--- include/linux/compat-3.12.h
+++ include/linux/compat-3.12.h
@@ -9,6 +9,13 @@
 #define PTR_ERR_OR_ZERO(p) PTR_RET(p)
 #endif
 
+#define MODULE_ALIAS_FS(NAME) MODULE_ALIAS("fs-" NAME)
+
+static inline struct inode *file_inode(struct file *f)
+{
+	return f->f_dentry->d_inode;
+}
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,12,0)) */
 
 #endif /* LINUX_3_12_COMPAT_H */
