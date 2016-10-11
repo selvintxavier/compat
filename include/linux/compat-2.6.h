369155893c884ef22d5b45d99c5510583aac2d8a
Steve Wise <swise@opengridcomputing.com>
2016-10-11 12:18:24 -0700

    compat-3.19: add debugfs_create_file_size() function
    Create compat/compat-3.19.c and add debugfs_create_file_size().
    
    Signed-off-by: Steve Wise <swise@opengridcomputing.com>

--- include/linux/compat-2.6.h
+++ include/linux/compat-2.6.h
@@ -75,6 +75,7 @@ void backport_dependency_symbol(void);
 #include <linux/compat-3.15.h>
 #include <linux/compat-3.16.h>
 #include <linux/compat-3.17.h>
+#include <linux/compat-4.0.h>
 #include <linux/compat-4.5.h>
 
 #endif /* LINUX_26_COMPAT_H */
---
+++ include/linux/compat-4.0.h
@@ -0,0 +1,31 @@
+#ifndef LINUX_4_0_COMPAT_H
+#define LINUX_4_0_COMPAT_H
+
+#include <linux/version.h>
+
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,0,0))
+
+#include <linux/dcache.h>
+
+/**
+ * d_inode - Get the actual inode of this dentry
+ * @dentry: The dentry to query
+ *
+ * This is the helper normal filesystems should use to get at their own inodes
+ * in their own dentries and ignore the layering superimposed upon them.
+ */
+static inline struct inode *d_inode(const struct dentry *dentry)
+{
+	return dentry->d_inode;
+}
+
+#define debugfs_create_file_size LINUX_BACKPORT(debugfs_create_file_size)
+
+struct dentry *debugfs_create_file_size(const char *name, umode_t mode,
+					struct dentry *parent, void *data,
+					const struct file_operations *fops,
+					loff_t file_size);
+
+#endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(4,0,0)) */
+
+#endif /* LINUX_4.0_COMPAT_H */
