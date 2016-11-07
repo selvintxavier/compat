876874e0066384dd8cbcb73e37ffffd197c92b8e
Vladimir Sokolovsky <vlad@mellanox.com>
2016-11-07 15:46:24 +0200

    compat-4.5.h: Added skb_inner_transport_offset
    Signed-off-by: Vladimir Sokolovsky <vlad@mellanox.com>

--- include/linux/compat-4.5.h
+++ include/linux/compat-4.5.h
@@ -11,6 +11,14 @@
 #define NETIF_F_CSUM_MASK NETIF_F_ALL_CSUM
 #endif
 
+#include <linux/skbuff.h>
+
+#define skb_inner_transport_offset LINUX_BACKPORT(skb_inner_transport_offset)
+static inline int skb_inner_transport_offset(const struct sk_buff *skb)
+{
+	return skb_inner_transport_header(skb) - skb->data;
+}
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(4,5,0)) */
 
 #endif /* LINUX_4_5_COMPAT_H */
