f6e653817ae39ddd1daf31292ec7b41bc8b46841
Tatyana Nikolova <tatyana.e.nikolova@intel.com>
2017-02-21 21:33:33 -0800

    compat/nes: Adding pci_zalloc_consistent backport
    Signed-off-by: Tatyana Nikolova <tatyana.e.nikolova@intel.com>

--- include/linux/compat-3.17.h
+++ include/linux/compat-3.17.h
@@ -3,8 +3,20 @@
 
 #include <linux/version.h>
 #include "../../compat/config.h"
+#include <linux/dma-mapping.h>
+#include <linux/pci.h>
+#include <linux/device.h>
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3,17,0))
+
+#define pci_zalloc_consistent backport_pci_zalloc_consistent
+static inline void *
+backport_pci_zalloc_consistent(struct pci_dev *hwdev, size_t size,
+                      dma_addr_t *dma_handle)
+{
+        return dma_zalloc_coherent(hwdev == NULL ? NULL : &hwdev->dev,
+				size, dma_handle, GFP_ATOMIC);
+}
 
 #ifndef HAVE_KTIME_GET_REAL_NS
 #include <linux/hrtimer.h>
